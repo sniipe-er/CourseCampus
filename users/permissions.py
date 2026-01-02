14e654065a64818776a9e528660de56cfac2a3d9
sniipe-er <ss.w.rengard@gmail.com>
2026-01-02 19:29:38 +0100

    feat(users-permissions): add role-based access control for students and instructors

--- users/permissions.py
+++ users/permissions.py
@@ -3,15 +3,9 @@ from rest_framework.permissions import BasePermission
 
 class IsInstructor(BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated and
-            request.user.role == 'instructor'
-        )
+        return request.user.is_authenticated and request.user.role == 'instructor'
 
 
 class IsStudent(BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated and
-            request.user.role == 'student'
-        )
+        return request.user.is_authenticated and request.user.role == 'student'
