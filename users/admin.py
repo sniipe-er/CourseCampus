e3d20a3c3e91f23be5d9a3b7d072352385d7c636
sniipe-er <ss.w.rengard@gmail.com>
2026-01-02 19:27:44 +0100

    feat(users-admin): configure custom User admin for email-based login

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import User
 
-# Register your models here.
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'role', 'is_staff', 'is_superuser')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'name', 'role')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'role', 'password1', 'password2'),
+        }),
+    )
