9e68ddf792cf9bb5788175e5ab942f152eb89cd4
sniipe-er <ss.w.rengard@gmail.com>
2026-03-26 22:43:39 +0100

    update

--- CourseCampus/urls.py
+++ CourseCampus/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -33,5 +34,6 @@ urlpatterns = [
     path('api/courses/', include('courses.urls')),
     path('api/enrollments/', include('enrollments.urls')),
     path('api/assignments/', include('assignments.urls')),
-    path('api/certificates/', include('certificates.urls')),
+path('api/certificates/', include('certificates.urls')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='frontend'),
 ]
