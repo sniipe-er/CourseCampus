99e2a28b0aed79b6cd65633cc621132a356504f6
sniipe-er <ss.w.rengard@gmail.com>
2026-03-31 01:21:22 +0100

    update

--- CourseCampus/urls.py
+++ CourseCampus/urls.py
@@ -1,39 +1,25 @@
 """
 URL configuration for CourseCampus project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.jwt')),
-
-    path('api/auth/jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
-    path('api/auth/jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
-
-    path('api/courses/', include('courses.urls')),
-    path('api/enrollments/', include('enrollments.urls')),
-    path('api/assignments/', include('assignments.urls')),
-    path('api/certificates/', include('certificates.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("djoser.urls")),
+    path("api/auth/", include("djoser.urls.jwt")),
+    path("api/auth/jwt/create/", TokenObtainPairView.as_view(), name="jwt-create"),
+    path("api/auth/jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
+    path("api/courses/", include("courses.urls")),
+    path("api/enrollments/", include("enrollments.urls")),
+    path("api/assignments/", include("assignments.urls")),
+    path("api/certificates/", include("certificates.urls")),
+    re_path(
+        r"^(?!api/|admin/|static/|assets/).*$",
+        TemplateView.as_view(template_name="index.html"),
+        name="frontend",
+    ),
 ]
